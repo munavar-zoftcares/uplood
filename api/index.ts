00d07b713283bda90ade9c5a551be8240d754107
munavarr <mnvrlogs@gmail.com>
2025-07-02 15:55:41 +0530

    committed

--- api/index.ts
+++ api/index.ts
@@ -33,23 +33,24 @@ app.get("/", (_req, res) => {
 //   fs.mkdirSync(uploadDir);
 // }
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, '../uploads'));
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
-const upload = multer({ storage });
-
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, path.join(__dirname, '../uploads'));
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + "-" + file.originalname);
+//   },
+// });
+// const upload = multer({ storage });
+const upload = multer({ storage: multer.memoryStorage() });
 app.post("/upload", upload.single("file"), (req, res) => {
   if (!req.file) {
     return res.status(400).send("No file uploaded.");
   }
   try {
     const filePath = req.file?.path;
-    const workbook = xlsx.readFile(filePath);
+    // const workbook = xlsx.readFile(filePath);
+    const workbook = xlsx.read(req.file.buffer, { type: 'buffer' });
     const sheetName = workbook.SheetNames[0];
     const worksheet = workbook.Sheets[sheetName];
     const sheetData = xlsx.utils.sheet_to_json(worksheet, {
